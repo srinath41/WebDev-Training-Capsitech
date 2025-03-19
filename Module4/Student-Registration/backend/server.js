853495e29e5de844839960b2475c3ce780c7d0f7
Srinath <devarapallisrinath8@gmail.com>
2025-03-19 16:06:05 +0530

    Task 4 - working with MangoDB and Express

--- Module4/Student-Registration/backend/server.js
+++ Module4/Student-Registration/backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import Student from "./models/Student.js";
+import Student from "./models/student.js";
 
 dotenv.config();
 const app = express();
